f261ac6f745f2d9a1f6e9b845ae2525fb7d75f7d
TuNombreDeUsuarioGitHub <tu_email@example.com>
2025-02-10 14:23:47 -0500

    funciones de redirecionamiento

--- app.py
+++ app.py
@@ -10,6 +10,22 @@ def login():
 def inicio():
     return render_template("inicio.html", title="Inicio")
 
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html",title="Usuarios")
+
+@app.route("/actividades")
+def actividades():
+    return render_template("actividades.html",title="Actividades")
+
+@app.route("/recursos")
+def recursos():
+    return render_template("recursos.html",title="Recursos")
+
+@app.route("/soporte")
+def soporte():
+    return render_template("soporte.html",title="Soporte")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
