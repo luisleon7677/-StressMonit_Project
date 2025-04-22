47fc823e685a30f10463a9185bddf8420d1c212d
TuNombreDeUsuarioGitHub <tu_email@example.com>
2025-04-22 15:25:00 -0500

    cambio de pruerto para despligue

--- app.py
+++ app.py
@@ -339,4 +339,5 @@ def eliminar_actividad(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
